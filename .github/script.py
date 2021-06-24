597d4ff337e39f4ac0379a5b65c93ca0b0a20a31
Pedro <31702537+PedrosUsername@users.noreply.github.com>
2021-06-24 16:23:12 -0300

    domain imgur.com removed from request cause bug

--- .github/script.py
+++ .github/script.py
@@ -9,7 +9,7 @@ sub = str(sys.argv[1])
 today = datetime.now().strftime('%d-%b-%Y %H:%M:%S')
 
 def getPushshiftData(sub):
-    url = 'https://api.pushshift.io/reddit/search/submission/?subreddit='+ str(sub) +'&after=7d&sort_type=num_comments&sort=desc&domain=i.redd.it&domain=i.imgur.com&size=1'
+    url = 'https://api.pushshift.io/reddit/search/submission/?subreddit='+ str(sub) +'&after=7d&sort_type=num_comments&sort=desc&domain=i.redd.it&size=1'
     r = requests.get(url)
     data = json.loads(r.text)
 
